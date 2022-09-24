865c00395d905a9908d14e4b15b7f9b1762a6f97
wenfeng <wenfengyangchn@gmail.com>
2022-09-24 23:47:26 +0200

    Update DepthFirstGraph and BreathFirstGraph

---
+++ Leetcode/6.z-字形变换.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode.cn id=6 lang=python3
+#
+# [6] Z 字形变换
+#
+
+# @lc code=start
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if not s:
+            return ""
+        if numRows == 1:
+            return s
+        if numRows == 2:
+«# @lc code=end
