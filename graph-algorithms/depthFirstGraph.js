3ba2e67f4c0c85068145189d8a3df0de83bee4a5
wenfeng <wenfengyangchn@gmail.com>
2022-09-28 00:00:50 +0200

    update graph algorithmus

--- graph-algorithms/depthFirstGraph.js
+++ graph-algorithms/depthFirstGraph.js
@@ -1,20 +1,20 @@
-// const depthFirstPrint = (graph, source) => {
-//     const stack = [ source ];
-//     while (stack.length > 0) {
-//         const current = stack.pop();
-//         console.log(current);
-//         for (let neighbor of graph[current]){
-//             stack.push(neighbor);
-//         }
-//     }
-// };
-
 const depthFirstPrint = (graph, source) => {
-    console.log(source);
-    for( let neighbor of graph[source]) {
-        depthFirstPrint(graph,neighbor)
+    const stack = [ source ];
+    while (stack.length > 0) {
+        const current = stack.pop();
+        console.log(current);
+        for (let neighbor of graph[current]){
+            stack.push(neighbor);
+        }
     }
 };
+
+// const depthFirstPrint = (graph, source) => {
+//     console.log(source);
+//     for( let neighbor of graph[source]) {
+//         depthFirstPrint(graph,neighbor)
+//     }
+// };
 
 const breadthFirstPrint = (graph, source) => {
     const queue = [ source ];
